da52fde34f7703f4b4a0bd4a8ab3403c35b729fb
Qwertin02 <qwertin02@gmail.com>
2023-12-07 12:35:22 +0100

    code format

--- main.js
+++ main.js
@@ -1,22 +1,21 @@
 const names = [];
-const div = document.querySelector('div')
+const div = document.querySelector("div");
 
 const addName = (e) => {
- e.preventDefault();
- const input = document.querySelector('input');
- const newName = input.value;
- if (input.value.length) {
-  for (name of names) {
-   if (name === newName) {
-    alert("to już jest!!!!!!");
-    return
-   }
+  e.preventDefault();
+  const input = document.querySelector("input");
+  const newName = input.value;
+  if (input.value.length) {
+    for (name of names) {
+      if (name === newName) {
+        alert("to już jest!!!!!!");
+        return;
+      }
+    }
+    names.push(newName);
+    // console.log(names);
+    div.textContent += newName + ", ";
+    input.value = "";
   }
-  names.push(newName);
-  // console.log(names);
-  div.textContent += newName + ", ";
-  input.value = "";
- }
-
-}
-document.querySelector('button').addEventListener('click', addName)
+};
+document.querySelector("button").addEventListener("click", addName);
